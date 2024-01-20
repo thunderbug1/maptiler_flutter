cf5709cfbb7c5970948476c306846b0ccc80d87d
Alexander Balasch <bll@tgw-group.com>
2024-01-20 21:00:50 +0000

    added test cases

--- test/maptiler_flutter_test.dart
+++ test/maptiler_flutter_test.dart
@@ -32,5 +32,20 @@ void main() {
       final res = await MapTiler.geocodingAPI.searchByName("new york");
       expect(res.features.isNotEmpty, isTrue);
     });
+    test('searchByCoordinates', () async {
+      final res = await MapTiler.geocodingAPI.searchByCoordinates(
+          longitude: -4.4285085, latitude: 36.72130290415578);
+      expect(res.features.isNotEmpty, isTrue);
+    });
+    test('searchByCoordinates 2', () async {
+      final res = await MapTiler.geocodingAPI.searchByCoordinates(
+          longitude: -4.421636648476124,
+          latitude: 36.72130290415578,
+          limit: 1,
+          excludeTypes: true,
+          types: ["country", "region", "subregion", "municipality"]);
+      //-4.421636648476124%2C36.72130290415578&limit=10&types=country%2Cregion%2Csubregion%2Cmunicipality&excludeTypes=true&autocomplete=true
+      expect(res.features.isNotEmpty, isTrue);
+    });
   });
 }
